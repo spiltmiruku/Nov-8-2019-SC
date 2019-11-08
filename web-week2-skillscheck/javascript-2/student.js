ab9c0db0d42736a790f94be1d6ac1488793c243a
spiltmiruku <kitsunedg@gmail.com>
2019-11-08 14:10:41 -0700

    Finished HTML+CSS 1 & javascript 2, working on problem 15 in javascript 1 and problem 13 needs troubleshooting

--- web-week2-skillscheck/javascript-2/student.js
+++ web-week2-skillscheck/javascript-2/student.js
@@ -1,30 +1,30 @@
 //////////////////SETUP////////////////////
-// Open javascripot-2/index.html in the browser. You should see "An Ode to Code" and a small box below it.
+// Open javascript-2/index.html in the browser. You should see "An Ode to Code" and a small box below it.
 
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
-// Code Here
+let lovesCode = true;
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
 
-// Code Here
+let age = 25;
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
-// Code Here
+let name = 'Kate';
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
-// Code Here
+let phrase = 'lol';
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-// Code Here
+if (lovesCode === true ? 'I love code' : 'I do not love to code');
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
